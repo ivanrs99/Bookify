b1973180c56fecc95398354d9591b30a58044266
ivanrs99 <salgueirosivan@gmail.com>
2023-11-03 15:26:56 +0100

    Seguridad firebase

--- database/firebase.js
+++ database/firebase.js
@@ -1,29 +1,16 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAfnUclsTg8PZQ6SHalhkzw2PcekASC4gI",
-  authDomain: "bookify-1dff3.firebaseapp.com",
-  projectId: "bookify-1dff3",
-  storageBucket: "bookify-1dff3.appspot.com",
-  messagingSenderId: "656397056522",
-  appId: "1:656397056522:web:2eac096fe12250508dfdf3",
-  measurementId: "G-EFYJF7B04R",
-};
+import { firebaseConfig } from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 
-//const auth = getAuth(app);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
